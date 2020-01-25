3b4fab722e76be33aa8681c850bfb2eaf07a702e
Vinay Shenoy <vinaysshenoy@gmail.com>
2020-01-25 17:28:49 +0530

    Add hooks for test runs

--- quarantine/src/main/kotlin/com/vinaysshenoy/quarantine/QuarantineTestRunner.kt
+++ quarantine/src/main/kotlin/com/vinaysshenoy/quarantine/QuarantineTestRunner.kt
@@ -1,13 +1,30 @@
 package com.vinaysshenoy.quarantine
 
+import org.junit.runner.Description
+import org.junit.runner.notification.RunListener
 import org.junit.runner.notification.RunNotifier
 import org.junit.runners.BlockJUnit4ClassRunner
 import org.junit.runners.model.FrameworkMethod
 import org.slf4j.LoggerFactory
 
-class QuarantineTestRunner(clazz: Class<*>): BlockJUnit4ClassRunner(clazz) {
+class QuarantineTestRunner(clazz: Class<*>) : BlockJUnit4ClassRunner(clazz) {
 
     private val logger = LoggerFactory.getLogger(QuarantineTestRunner::class.java.simpleName)
+
+    override fun run(notifier: RunNotifier) {
+        notifier.addListener(object : RunListener() {
+            override fun testSuiteStarted(description: Description) {
+                logger.info("Start test run: $description with ${description.testCount()} tests")
+                super.testSuiteStarted(description)
+            }
+
+            override fun testSuiteFinished(description: Description?) {
+                super.testSuiteFinished(description)
+                logger.info("Completed test run!")
+            }
+        })
+        super.run(notifier)
+    }
 
     override fun runChild(method: FrameworkMethod, notifier: RunNotifier) {
         logger.info("${method.declaringClass.canonicalName} -> ${method.name}")
--- src/test/kotlin/com/vinaysshenoy/testrunnermvp/FlakyTest.kt
+++ src/test/kotlin/com/vinaysshenoy/testrunnermvp/FlakyTest.kt
@@ -16,4 +16,25 @@ class FlakyTest {
     fun `flaky test that fails 50% of the time`() {
         expectThat(random.nextFloat()) isGreaterThan 0.5F
     }
+
+    @Test
+    fun `flaky test that fails 30% of the time`() {
+        expectThat(random.nextFloat()) isGreaterThan 0.3F
+    }
+
+    @RunWith(QuarantineTestRunner::class)
+    class NestedFlakyTest {
+
+        private val random = Random()
+
+        @Test
+        fun `flaky test that fails 80% of the time`() {
+            expectThat(random.nextFloat()) isGreaterThan 0.8F
+        }
+
+        @Test
+        fun `flaky test that fails 70% of the time`() {
+            expectThat(random.nextFloat()) isGreaterThan 0.7F
+        }
+    }
 }
