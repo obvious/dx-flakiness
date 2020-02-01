392a3269fae1e6e42ffd87c6f1bf51e1286aced4
Vinay Shenoy <vinaysshenoy@gmail.com>
2020-02-01 16:30:02 +0530

    Add custom extension for resolving JDBI objects

--- quarantine-server/src/test/kotlin/com/vinaysshenoy/quarantine/dbqueries/QuarantineDaoTest.kt
+++ quarantine-server/src/test/kotlin/com/vinaysshenoy/quarantine/dbqueries/QuarantineDaoTest.kt
@@ -1,61 +1,22 @@
 package com.vinaysshenoy.quarantine.dbqueries
 
-import ch.qos.logback.classic.Level
 import com.vinaysshenoy.quarantine.dao.FlakyStatus
 import com.vinaysshenoy.quarantine.dao.QuarantineDao
-import com.vinaysshenoy.quarantine.mappers.jdbi.JdbiInstant
+import com.vinaysshenoy.quarantine.extensions.JdbiObjectParameterResolver
 import com.vinaysshenoy.quarantine.testCase
 import com.vinaysshenoy.quarantine.testRun
 import com.vinaysshenoy.quarantine.testRunResult
-import liquibase.Liquibase
-import liquibase.database.jvm.JdbcConnection
-import liquibase.resource.ClassLoaderResourceAccessor
-import org.jdbi.v3.core.Jdbi
-import org.jdbi.v3.core.kotlin.KotlinPlugin
-import org.jdbi.v3.sqlobject.SqlObjectPlugin
-import org.jdbi.v3.sqlobject.kotlin.KotlinSqlObjectPlugin
-import org.jdbi.v3.sqlobject.kotlin.onDemand
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
+import org.junit.jupiter.api.extension.ExtendWith
 import strikt.api.expectThat
 import strikt.assertions.containsExactlyInAnyOrder
 import strikt.assertions.isEqualTo
-import java.sql.Connection
-import java.sql.DriverManager
 
+@ExtendWith(JdbiObjectParameterResolver::class)
 class QuarantineDaoTest {
 
-    private val dbConnection: Connection = DriverManager.getConnection("jdbc:sqlite::memory:")
-
-    private val jdbi: Jdbi = Jdbi.create(dbConnection)
-        .apply {
-            installPlugin(SqlObjectPlugin())
-            installPlugin(KotlinPlugin())
-            installPlugin(KotlinSqlObjectPlugin())
-            JdbiInstant.install(this)
-        }
-
-    private val quarantineDao: QuarantineDao by lazy(LazyThreadSafetyMode.NONE) {
-        jdbi.onDemand<QuarantineDao>()
-    }
-
-    @BeforeEach
-    fun setUp() {
-        val liquibase = Liquibase(
-            "migrations.sql",
-            ClassLoaderResourceAccessor(),
-            JdbcConnection(dbConnection)
-        )
-
-        val logger = LoggerFactory.getLogger("liquibase")
-        (logger as ch.qos.logback.classic.Logger).level = Level.OFF
-
-        liquibase.update("")
-    }
-
     @Test
-    fun `saving test results should work as expected`() {
+    fun `saving test results should work as expected`(quarantineDao: QuarantineDao) {
         // given
         val testRun = testRun(id = -1)
         val testCases = listOf(
---
+++ quarantine-server/src/test/kotlin/com/vinaysshenoy/quarantine/extensions/JdbiObjectParameterResolver.kt
@@ -0,0 +1,79 @@
+package com.vinaysshenoy.quarantine.extensions
+
+import ch.qos.logback.classic.Level
+import com.vinaysshenoy.quarantine.mappers.jdbi.JdbiInstant
+import liquibase.Liquibase
+import liquibase.database.jvm.JdbcConnection
+import liquibase.resource.ClassLoaderResourceAccessor
+import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.kotlin.KotlinPlugin
+import org.jdbi.v3.sqlobject.SqlObjectPlugin
+import org.jdbi.v3.sqlobject.kotlin.KotlinSqlObjectPlugin
+import org.junit.jupiter.api.extension.AfterEachCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.ParameterContext
+import org.junit.jupiter.api.extension.ParameterResolver
+import org.slf4j.LoggerFactory
+import java.sql.Connection
+import java.sql.DriverManager
+
+class JdbiObjectParameterResolver : ParameterResolver, AfterEachCallback {
+
+    companion object {
+        private val EXTENSION_NAMESPACE = ExtensionContext.Namespace.create(Any())
+        private val KEY = Any()
+    }
+
+    override fun supportsParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext): Boolean {
+        // This is fine because JDBI will take care of the actual checking in resolveParameter()
+        return true
+    }
+
+    override fun resolveParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext): Any {
+        val dbConnection = DriverManager.getConnection("jdbc:sqlite::memory:")
+
+        setupDatabase(dbConnection, extensionContext)
+
+        val jdbi = Jdbi.create(dbConnection)
+            .apply {
+                installPlugin(SqlObjectPlugin())
+                installPlugin(KotlinPlugin())
+                installPlugin(KotlinSqlObjectPlugin())
+                JdbiInstant.install(this)
+            }
+
+        val type = parameterContext.parameter.type
+
+        return jdbi.onDemand(type)
+    }
+
+    override fun afterEach(context: ExtensionContext) {
+        cleanUp(context)
+    }
+
+    private fun cleanUp(context: ExtensionContext) {
+        context
+            .getStore(EXTENSION_NAMESPACE)
+            .remove(KEY, Liquibase::class.java)?.database?.close()
+    }
+
+    private fun setupDatabase(
+        connection: Connection,
+        context: ExtensionContext
+    ) {
+        val liquibase = Liquibase(
+            "migrations.sql",
+            ClassLoaderResourceAccessor(),
+            JdbcConnection(connection)
+        )
+
+        val logger = LoggerFactory.getLogger("liquibase")
+        (logger as ch.qos.logback.classic.Logger).level = Level.OFF
+
+        liquibase.update("")
+
+        context
+            .getStore(EXTENSION_NAMESPACE)
+            .put(KEY, liquibase)
+    }
+}
