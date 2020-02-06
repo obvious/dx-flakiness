c4a736c05672086a1b334b72bcef88275e5fe42f
Vinay Shenoy <vinaysshenoy@gmail.com>
2020-02-06 20:50:41 +0530

    Extract concrete `QuarantinedStatement` class

--- quarantine/src/main/kotlin/com/vinaysshenoy/quarantine/QuarantineTestRule.kt
+++ quarantine/src/main/kotlin/com/vinaysshenoy/quarantine/QuarantineTestRule.kt
@@ -8,67 +8,75 @@ class QuarantineTestRule : TestRule {
 
     private val repository: TestRepository by lazy { InMemoryTestRepository.instance() }
 
-    private val logger = logger<QuarantineTestRule>()
-
     init {
         ReportFlakyTestsOnComplete.setup(repository)
     }
 
-    private val flakyTestRetryCount = 10
-
     override fun apply(statement: Statement, description: Description): Statement {
-        return object : Statement() {
+        return QuarantinedStatement(
+            repository = repository,
+            flakyTestRetryCount = 10,
+            base = statement,
+            description = description
+        )
+    }
 
-            override fun evaluate() {
-                val descriptors = TestDescriptor.fromDescription(description)
-                repository.add(descriptors)
+    private class QuarantinedStatement(
+        private val repository: TestRepository,
+        private val flakyTestRetryCount: Int,
+        private val base: Statement,
+        private val description: Description
+    ): Statement() {
 
-                if (description.isTest) {
-                    val testClazzName = description.className
-                    val testMethodName = description.methodName
+        override fun evaluate() {
+            val descriptors = TestDescriptor.fromDescription(description)
+            repository.add(descriptors)
 
-                    try {
-                        statement.evaluate()
-                        // If it passes, mark it as not-flaky
-                        repository.record(testClazzName, testMethodName, false)
-                    } catch (e: Throwable) {
-                        // This test having failed once need not mean that is flaky. We will try running the test some more times
-                        // and see if they pass in any of those. In which case, we will mark them as flaky.
-                        val testRunCount = generateSequence(1) { testRetryNumber ->
-                            when {
-                                testRetryNumber > flakyTestRetryCount -> null
-                                else -> {
-                                    val hasPassedOnRetry = retryAndReturnException(statement) == null
+            if (description.isTest) {
+                val testClazzName = description.className
+                val testMethodName = description.methodName
 
-                                    if (hasPassedOnRetry) null else testRetryNumber + 1
-                                }
+                try {
+                    base.evaluate()
+                    // If it passes, mark it as not-flaky
+                    repository.record(testClazzName, testMethodName, false)
+                } catch (e: Throwable) {
+                    // This test having failed once need not mean that is flaky. We will try running the test some more times
+                    // and see if they pass in any of those. In which case, we will mark them as flaky.
+                    val testRunCount = generateSequence(1) { testRetryNumber ->
+                        when {
+                            testRetryNumber > flakyTestRetryCount -> null
+                            else -> {
+                                val hasPassedOnRetry = retryAndReturnException(base) == null
+
+                                if (hasPassedOnRetry) null else testRetryNumber + 1
                             }
-                        }.last()
-
-                        if (testRunCount == flakyTestRetryCount) {
-                            // We have tested it for all retry counts and it has failed each time, so it is not a flaky test
-                            repository.record(testClazzName, testMethodName, false)
-
-                            // Only report failed, non-flaky tests as failures
-                            throw e
-                        } else {
-                            repository.record(testClazzName, testMethodName, true)
                         }
+                    }.last()
+
+                    if (testRunCount == flakyTestRetryCount) {
+                        // We have tested it for all retry counts and it has failed each time, so it is not a flaky test
+                        repository.record(testClazzName, testMethodName, false)
+
+                        // Only report failed, non-flaky tests as failures
+                        throw e
+                    } else {
+                        repository.record(testClazzName, testMethodName, true)
                     }
                 }
             }
         }
-    }
 
-    private fun retryAndReturnException(statement: Statement): Throwable? {
-        var failure: Throwable? = null
+        private fun retryAndReturnException(statement: Statement): Throwable? {
+            var failure: Throwable? = null
 
-        try {
-            statement.evaluate()
-        } catch (e: Throwable) {
-            failure = e
+            try {
+                statement.evaluate()
+            } catch (e: Throwable) {
+                failure = e
+            }
+
+            return failure
         }
-
-        return failure
     }
 }
