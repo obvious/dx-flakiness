d4e52fe1ce7186bac70c4a7792e15528aefb600c
Vinay Shenoy <vinaysshenoy@gmail.com>
2020-03-30 09:00:12 +0530

    Add `Quarantine` singleton class

---
+++ quarantine/src/main/kotlin/com/vinaysshenoy/quarantine/Quarantine.kt
@@ -0,0 +1,11 @@
+package com.vinaysshenoy.quarantine
+
+object Quarantine {
+    
+    var classLoader: ClassLoader = ClassLoader.getSystemClassLoader()
+
+    val repository: TestRepository by lazy { InMemoryTestRepository.instance(classLoader) }
+
+    val isEnabled: Boolean
+        get() = repository.config().enabled
+}
